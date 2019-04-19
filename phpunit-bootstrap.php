80e281777b12aa35350900c8cff9ac1ad379564b
David Moreno Cortina <davidmorenocortina@gmail.com>
2019-04-19 14:15:50 +0200

    Tests fix

--- phpunit-bootstrap.php
+++ phpunit-bootstrap.php
@@ -1,3 +1,31 @@
 <?php
 
+use DavidMorenoCortina\JWT\Config\DBInitialization;
+use DavidMorenoCortina\JWT\Exception\DBException;
+
 require __DIR__ . '/vendor/autoload.php';
+
+$settings = require __DIR__ . '/phpunit-settings.php';
+
+$dsn = 'mysql:host=' . $settings['db']['host'] . ';port=' . $settings['db']['port'] . ';dbname=' . $settings['db']['dbName'];
+$conn = new PDO($dsn, $settings['db']['user'], $settings['db']['password'], [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8']);
+
+$dbInitialization = new DBInitialization();
+
+$dbInitialization->createRSATable($conn);
+
+try {
+    $dbInitialization->createRsaKey($conn, 'tests');
+} catch (DBException $e) {
+    echo "Could not initialize the database\n";
+    die();
+}
+
+$dbInitialization->createUserTable($conn);
+
+try {
+    $dbInitialization->createUser($conn, 'demo', 'test', true);
+} catch (DBException $e) {
+    echo "Could not initialize the database\n";
+    die();
+}
